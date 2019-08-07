90ccbb2f5ae49bf56bcb6086955d6e68092a9319
MozhuGithub <932341874@qq.com>
2019-08-07 14:02:44 +0800

    Site updated: 2019-08-07 14:02:42

--- js/src/clipboard-user.js
+++ js/src/clipboard-user.js
@@ -1,17 +1,18 @@
-/*ҳ��������ɺ󣬴������ư�ť*/
-!function (e, t, a) { 
-  /* code */
-  var initCopyCode = function(){
-    var copyHtml = '';
-    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
-    copyHtml += '</button>';
-    $(".highlight .code pre").before(copyHtml);
-    new ClipboardJS('.btn-copy', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
-    });
-  }
-  initCopyCode();
-}(window, document);
+  /*ҳ��������ɺ󣬴������ư�ť*/
+  !function (e, t, a) { 
+    /* code */
+    var initCopyCode = function(){
+      var copyHtml = '';
+      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+      //fa fa-globe����ȥ������滻�Լ���Ҫ��ͼ��
+copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
+      copyHtml += '</button>';
+      $(".highlight .code pre").before(copyHtml);
+      new ClipboardJS('.btn-copy', {
+          target: function(trigger) {
+              return trigger.nextElementSibling;
+          }
+      });
+    }
+    initCopyCode();
+  }(window, document);
